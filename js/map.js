485a1f576ff6dc7098266f884d712d38ae2d8490
cartosquare <1517374954@qq.com>
2016-04-24 00:27:07 +0800

    开放地图页面。

--- js/map.js
+++ js/map.js
@@ -8,16 +8,16 @@ $.get('/backend/visit', function (data) {
     console.log('你是第' + data.key + '个制图者！');
     mapid = parseInt(data.key);
 
-    tileLayer = L.tileLayer('/tile/' + mapid + '/{z}/{x}/{y}&{tileID}', {
+    tileLayer = L.tileLayer('/tile/' + mapid + '/{z}/{x}/{y}&retina=1&{tileID}', {
         attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors>',
         zoomControl: false,
         tileID: function () { return tileVersion; }
     }).addTo(mymap);
 
     L.control.geocoder('search-Eg9AZ2m', {
-        bounds: true,
-        position: 'topright',
-        expanded: true,
+        //bounds: true,
+        //position: 'topright',
+        //expanded: true,
         placeholder: '搜索地点'
     }).addTo(mymap);
 });
